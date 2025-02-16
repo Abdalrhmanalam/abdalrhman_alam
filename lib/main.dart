c59fa86b44ae370ec4c8a198b489a026d655836e
Abdalrhmanalam <ma0773721@gmail.com>
2025-02-16 22:09:48 +0300

    getx

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         home: Maps(),
         //  Task(),
         //  SplashScreen(),
-        // GalleryAccess(),
+          GalleryAccess(),
         // DateTimePicker(),
         // SliderDemo(),
         //PageViewDemo(),
